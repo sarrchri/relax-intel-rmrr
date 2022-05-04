422321c64ca9d4bb2c75b63ac64deac03e4b909e
Aterfax <Aterfax@users.noreply.github.com>
2022-05-04 18:13:03 +0100

    Create build7.1-12.sh

---
+++ build/proxmox/build7.1-12.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+export PVE_KERNEL_BRANCH=master
+export RELAX_INTEL_GIT_REPO="https://github.com/Aterfax/relax-intel-rmrr.git"
+export RELAX_PATCH="add-relaxable-rmrr-5_15.patch"
+export PROXMOX_PATCH="proxmox7.patch"
+
+./build.sh
