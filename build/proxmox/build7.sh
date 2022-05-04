6e515f57ab1f4167b2d3888ecbf3bb9e8e46f7ce
Aterfax <Aterfax@users.noreply.github.com>
2022-05-04 18:13:48 +0100

    Update build7.sh

--- build/proxmox/build7.sh
+++ build/proxmox/build7.sh
@@ -2,7 +2,7 @@
 set -e
 
 export PVE_KERNEL_BRANCH=pve-kernel-5.13
-export RELAX_INTEL_GIT_REPO="https://github.com/jamestutton/relax-intel-rmrr.git"
+export RELAX_INTEL_GIT_REPO="https://github.com/Aterfax/relax-intel-rmrr.git"
 export RELAX_PATCH="add-relaxable-rmrr-5_11.patch"
 export PROXMOX_PATCH="proxmox7.patch"
 
