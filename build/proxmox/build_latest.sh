fd19dd0353ba68a9ca832fac6705a3542615798f
sarrchri <102296983+sarrchri@users.noreply.github.com>
2023-01-01 20:30:44 +0200

    Update build_latest.sh

--- build/proxmox/build_latest.sh
+++ build/proxmox/build_latest.sh
@@ -2,7 +2,7 @@
 set -e
 
 export PVE_KERNEL_BRANCH=pve-kernel-5.15
-export RELAX_INTEL_GIT_REPO="https://github.com/Aterfax/relax-intel-rmrr.git"
+export RELAX_INTEL_GIT_REPO="https://github.com/sarrchri/relax-intel-rmrr.git"
 export RELAX_PATCH="add-relaxable-rmrr-5_15.patch"
 export PROXMOX_PATCH="proxmox7.patch"
 
