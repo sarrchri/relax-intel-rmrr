7fc26717c89000b75c3e0967cb7471669600717c
sarrchri <christian@sarre.eu>
2023-02-07 09:19:23 +0200

    Add variable for Ubuntu Kernel branch

--- build/proxmox/build_kernel-5.19.sh
+++ build/proxmox/build_kernel-5.19.sh
@@ -2,6 +2,7 @@
 set -e
 
 export PVE_KERNEL_BRANCH=pve-kernel-5.19
+export PVE_UBUNTU_KERNEL_MIRROR_BRANCH=Ubuntu-5.19.0-31.32
 export RELAX_INTEL_GIT_REPO="https://github.com/sarrchri/relax-intel-rmrr.git"
 export RELAX_PATCH="add-relaxable-rmrr-5_15.patch"
 export PROXMOX_PATCH="proxmox7.patch"
