7a13076a919c01fac24edd0f6685a1777007d15f
sarrchri <christian@sarre.eu>
2023-02-07 10:03:11 +0200

    Revert "Add variable for Ubuntu Kernel branch".
    It wasn't needed after all.

--- build/proxmox/build_kernel-5.15.sh
+++ build/proxmox/build_kernel-5.15.sh
@@ -2,7 +2,6 @@
 set -e
 
 export PVE_KERNEL_BRANCH=pve-kernel-5.15
-export PVE_UBUNTU_KERNEL_MIRROR_BRANCH=Ubuntu-5.15.0-65.72
 export RELAX_INTEL_GIT_REPO="https://github.com/sarrchri/relax-intel-rmrr.git"
 export RELAX_PATCH="add-relaxable-rmrr-5_15.patch"
 export PROXMOX_PATCH="proxmox7.patch"
